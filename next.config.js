e92972b989d58d00ee89bfecee0ac9c5f54148f5
Stanley Nzegwu <nzegwustanley@gmail.com>
2023-05-17 18:26:39 +0100

    new commit

--- next.config.js
+++ next.config.js
@@ -1,23 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//   appDir: true,
-//   serverComponentsExternalPackages: ["mongoose"],
-//   },
-//   images: {
-//   domains: ['images.pexels.com','d205bpvrqc9yn1.cloudfront.net','i.ytimg.com','lh3.googleusercontent.com'],
-//   },
-//   webpack(config) {
-//   config.experiments = {
-//   ...config.experiments,
-//   topLevelAwait: true,
-//   };
-//   return config;
-//   },
-//   };
-  
-//   module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
